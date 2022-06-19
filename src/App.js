750e25355bbf6bdd32b69be48ac7d5db68e384ef
Nehemiah27 <96545668+Nehemiah27@users.noreply.github.com>
2022-06-19 15:43:03 +0530

    ClipboardOption

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function App() {
   const [answer, setAnwser] = useState("He who will never try, will never get");
+  const [showIt, setShowIt] = useState("");
   const newData = function dating() {
+    setShowIt("");
     fetch("https://api.quotable.io/random")
       .then((response) => {
         return response.json();
@@ -11,11 +14,21 @@ export default function App() {
         setAnwser(data.content);
       });
   };
+  function getIt() {
+    setShowIt(" Now you also have it, try checking your Clipboard");
+  }
   return (
     <>
       <h1>{answer}</h1>
       <div />
       <button onClick={newData}>Click here to Load New Quote</button>
+      <div />
+      <br />
+      <CopyToClipboard text={answer}>
+        <button onClick={getIt}>Get it</button>
+      </CopyToClipboard>
+
+      <span style={{ color: "green" }}>{showIt}</span>
     </>
   );
 }
